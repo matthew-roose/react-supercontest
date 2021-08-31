0a4b0bbea97265f99c5eb4965a9e8ae2dd53bfdc
Matthew Roose <mattr1@vt.edu>
2021-08-31 17:14:46 -0400

    Added GigaChad to Welcome screen

--- src/screens/Welcome/Welcome.js
+++ src/screens/Welcome/Welcome.js
@@ -1,3 +1,25 @@
+import React from 'react';
+
+import classes from './Welcome.module.css';
+
 export const Welcome = () => {
-  return <div>2:23:45:23 until picks lock!</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.background}></div>
+      <div className={classes.welcomeText}>
+        <p>Hey brahs, welcome to my supercontest.</p>
+        <p>A few things:</p>
+        <p>
+          New games will be posted every Wednesday. To play, make 5 picks
+          against the spread every week.
+        </p>
+        <p>
+          Passwords are definitely not handled securely so don't use your bank
+          password.
+        </p>
+        <p>Good luck and have fun!</p>
+        <p>- Chad</p>
+      </div>
+    </div>
+  );
 };
